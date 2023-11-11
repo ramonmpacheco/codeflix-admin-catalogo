978668557466adac7925cf8b8f03b0ef69e952ed
Ramon Pacheco <rmpacheco2@gmail.com>
2023-11-11 20:31:33 -0300

    add: initial infra

---
+++ infrastructure/src/main/java/com/codeflix/admin/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.codeflix.admin.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.codeflix.admin.catalogo")
+public class WebServerConfig {
+}
