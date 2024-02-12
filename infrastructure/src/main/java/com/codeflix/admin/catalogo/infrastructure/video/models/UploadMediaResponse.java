b06da9d8f4fe86f2682f192dfbe56213fc7107cc
Ramon Pacheco <rmpacheco2@gmail.com>
2024-02-12 11:44:51 -0300

    add: video api

---
+++ infrastructure/src/main/java/com/codeflix/admin/catalogo/infrastructure/video/models/UploadMediaResponse.java
@@ -0,0 +1,10 @@
+package com.codeflix.admin.catalogo.infrastructure.video.models;
+
+import com.codeflix.admin.catalogo.domain.video.VideoMediaType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record UploadMediaResponse(
+        @JsonProperty("video_id") String videoId,
+        @JsonProperty("media_type") VideoMediaType mediaType
+) {
+}
